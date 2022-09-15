99dac84fa57e647e8a35c57fc8b528d810c590db
JPechberty <j.pechberty@gmail.com>
2022-09-15 21:10:36 +0200

    Clean up

--- app/public/secure-form/action.php
+++ app/public/secure-form/action.php
@@ -1,33 +1,21 @@
 <?php
-
 require __DIR__ . '/../../vendor/autoload.php';
 
 use App\Entity\User;
 use App\Model\userModel;
 use Dotenv\Dotenv;
-
 // Gestion des fichiers environnement
 $dotenv = Dotenv::createImmutable(__DIR__ . "/../../");
 $dotenv->load();
-$errors = [];
-$success = true;
-
-$email = $_POST['email'];
-$name = $_POST['name'];
-$password = $_POST['password'];
-
-if ($email === "") {
-    $errors["email"] = "Empty email";
-}
 
 
-if (count(array_keys($errors)) === 0) {
-    $user = new User(null, $email, $name, $password);
+if (isset($_POST['submit'])) {
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $password = $_POST['password'];
+
+    $user = new User(null,$email,$name,$password);
     $userModel = new UserModel();
     $userModel->create($user);
-} else {
-    $success = false;
+    header("Location: ../secure-form/index.php");
 }
-
-
-echo json_encode(["success" => $success, "errors" => $errors]);
